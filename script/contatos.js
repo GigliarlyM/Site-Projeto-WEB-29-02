1665a6e814fd1e4a80fe96627a2cd35608e1b475
Daniel <daniellsoouza380@gmail.com>
2024-06-24 13:26:48 -0300

    add alert contato / start update locadora

--- script/contatos.js
+++ script/contatos.js
@@ -13,3 +13,15 @@ function nextImage() {
 
     document.getElementById("radio" + count).checked = true;
 }
+
+let btn = document.getElementById('btn-form')
+
+btn.onclick = function() {
+    let nome = document.getElementById('inputName').value
+    let email = document.getElementById('inputEmail').value
+    let nota =  document.querySelector("input[name='avaliacao']:checked").value
+
+
+    alert(`Sua avaliação ajuda a melhorar nossos serviços, obrigado!
+    \nSua avaliação: \nNome: ${nome}\nEmail: ${email}\nNota: ${nota}`)
+}
